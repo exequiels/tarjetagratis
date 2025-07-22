32a7f077947bdaad7975d1e44b13cd96b117533c
Exequiel Sabatie <exequiel.sabatie@enarvi.com>
2025-07-22 16:31:54 -0300

    no message

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <div className="col-12">
       <div className="border-round-lg centrado p-3">
         <div className="mb-2 md:mb-0 flex gap-1">
-          © 2025 Exequiel
+          © 2025
           <a
             href="https://sabatie.com.ar"
             target="_blank"
