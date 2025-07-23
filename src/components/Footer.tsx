0b43dee497005c3b7b77fa5604fef26679006a22
Exequiel Sabatie <exequiel.sabatie@enarvi.com>
2025-07-23 12:04:20 -0300

    messarround

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,14 @@ const Footer = () => {
     <div className="col-12">
       <div className="border-round-lg centrado p-3">
         <div className="mb-2 md:mb-0 flex gap-1">
-          © 2025 TheGarageKid Project
+          © 2025
+          <a
+            href="https://sabatie.com.ar"
+            target="_blank"
+            className="underline"
+          >
+            Sabatie
+          </a>
         </div>
       </div>
     </div>
